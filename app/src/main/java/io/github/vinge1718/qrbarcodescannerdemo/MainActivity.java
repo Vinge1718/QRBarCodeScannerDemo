0135c4379d7ada772bf5d1d21c282d9b1c0e0e5f
victor njonge <vinge1718@gmail.com>
2018-10-22 20:00:38 +0300

    created the two functions that I have just used to check for permission and also request for permission - the check for permission returns a boolean and the request for permission prompts a dialogue

--- app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/MainActivity.java
+++ app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/MainActivity.java
@@ -1,18 +1,22 @@
 package io.github.vinge1718.qrbarcodescannerdemo;
 
-
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Build;
-
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-
 
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-
+import static android.Manifest.permission.CAMERA;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
@@ -36,7 +40,38 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         }
     }
 
+    private boolean checkPermission(){
+        return (ContextCompat.checkSelfPermission(MainActivity.this, CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
 
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    public void onRequestPermisionsResult(int requestCode, String permission[], int grantResults[]){
+        switch (requestCode)
+        {
+            case REQUEST_CAMERA:
+                if(grantResults.length > 0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Permissions Denied", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            if(shouldShowRequestPermissionRationale(CAMERA)){
+
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    public void displayAlertMessage(String message, DialogInterface.OnCancelListener listener){
+        new AlertDialog()
+    }
 
     @Override
     public void handleResult(Result result) {
