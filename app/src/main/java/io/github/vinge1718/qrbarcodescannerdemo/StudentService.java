5dc064097fabe5469b77d6d5aa11167e9aad1aad
Vinge1718 <vinge1718@gmail.com>
2018-11-03 00:08:04 +0300

    created a service for fetching the students details

--- app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/StudentService.java
+++ app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/StudentService.java
@@ -1,4 +1,19 @@
 package io.github.vinge1718.qrbarcodescannerdemo;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class StudentService {
+    public static OkHttpClient client = new OkHttpClient();
+
+    public static void findStudent(String scanResult, Callback callback){
+        Request request = new Request.Builder()
+                .url("https://moringa-attendance.herokuapp.com/api/v1/get-user/"+scanResult)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
 }
