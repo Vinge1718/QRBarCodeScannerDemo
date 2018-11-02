36edd410401381004641d0ea50eb435e5e3a9af9
Vinge1718 <vinge1718@gmail.com>
2018-11-03 00:09:15 +0300

    created the model for parsing or casting the details fethed from the API

--- app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/Student.java
+++ app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/Student.java
@@ -1,4 +1,25 @@
 package io.github.vinge1718.qrbarcodescannerdemo;
 
 public class Student {
+    private String mName;
+    private String mImageUrl;
+    private String mClass;
+
+    public Student(String studentName, String imageUrl, String studentClass){
+        this.mName = studentName;
+        this.mClass = studentClass;
+        this.mImageUrl = imageUrl;
+    }
+
+    public String getStudentName(){
+        return mName;
+    }
+
+    public String getClasses(){
+        return mClass;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
 }
