f070cfa1fb2a4b97beec09acd2c97e2251dbd03d
Vinge1718 <vinge1718@gmail.com>
2018-11-03 11:46:12 +0300

    Added the student service service to process the received json file filw from the API

--- app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/StudentService.java
+++ app/src/main/java/io/github/vinge1718/qrbarcodescannerdemo/StudentService.java
@@ -1,9 +1,16 @@
 package io.github.vinge1718.qrbarcodescannerdemo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class StudentService {
     public static OkHttpClient client = new OkHttpClient();
@@ -15,5 +22,27 @@ public class StudentService {
 
         Call call = client.newCall(request);
         call.enqueue(callback);
+    }
+
+    public ArrayList<Student> processResults(Response response){
+        ArrayList<Student> students = new ArrayList<>();
+        
+        try{
+            String jsonData = response.body().string();
+            JSONObject users = new JSONObject(jsonData);
+
+            String studentName = users.getString("name");
+            String studentClass = users.getString("class");
+            String imageUrl = users.getString("image");
+            Student student = new Student(studentName, studentClass, imageUrl);
+            students.add(student);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        return students;
     }
 }
